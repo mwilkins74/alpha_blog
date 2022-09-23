be881b292ac13f79de73a823dfdcf2750f5e2456
Mark <markwilkins86@gmail.com>
2022-09-23 16:39:46 -0400

    Article Index Built

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
 
-    def index
-        render json: Article.all
-    end
-
     def show
         @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
     end
 
 end
